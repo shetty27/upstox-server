7c39d29f69b30d13f9089ce2a29c9c3a785fa74e
shetty27 <sevdakaushal@gmail.com>
2025-04-02 11:35:34 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 from fastapi import FastAPI
 from upstox_api import fetch_stock_data
-from database import save_to_db
+from database import save_to_firebase
 
 app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Stock Prediction API is Running!"}
+    return {"message": "Stock Prediction API is Running with Firebase!"}
 
 @app.get("/fetch/{symbol}")
 def get_stock(symbol: str):
     price = fetch_stock_data(symbol)
     if price:
-        save_to_db(symbol, price)
+        save_to_firebase(symbol, price)
         return {"symbol": symbol, "price": price}
     return {"error": "Failed to fetch data"}
